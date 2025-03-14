4a31a623c0e86fe599aad7d1fc9f79d0b6e9ad4e
Weishi Li <liweishi@kylinos.cn>
2025-03-14 16:31:32 +0800

    solve tests-2

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,17 +27,23 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
-
+#[link(name = "foo")]
 extern "Rust" {
+
     fn my_demo_function(a: u32) -> u32;
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
-mod Foo {
+mod foo {
     // No `extern` equals `extern "Rust"`.
+    #[no_mangle]
     fn my_demo_function(a: u32) -> u32 {
         a
+    }
+
+    #[no_mangle]
+    fn my_demo_function_alias(a: u32) -> u32 {
+        my_demo_function(a)
     }
 }
 
