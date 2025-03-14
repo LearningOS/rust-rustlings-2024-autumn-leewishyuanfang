ee342f859ff9cc5a29de95b3c0bcdfe7ea40516f
Weishi Li <liweishi@kylinos.cn>
2025-03-14 09:53:07 +0800

    solve conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut person = Person::default();
+        if s.len() > 0 {
+            let v: Vec<&str> = s.split(",").collect();
+            if v.len() == 2 {
+                if v[0].len() > 0 {
+                    
+                    match v[1].parse() {
+                        Ok(age) => {
+                            person.name = v[0].to_string();
+                            person.age = age;
+                        },
+                        Err(_) => {},
+                    };
+                }
+            }
+        }
+        person
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,31 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+       
+        if s.len() > 0 {
+            let v: Vec<&str> = s.split(",").collect();
+            if v.len() == 2 {
+                if v[0].len() > 0 {
+                    match v[1].parse() {
+                        Ok(age) => {
+                            Ok(Person {
+                                name: v[0].to_string(),
+                                age: age,
+                            })
+                        },
+                        Err(err) => {
+                            Err(ParsePersonError::ParseInt(err))
+                        },
+                    }
+                } else {
+                    Err(ParsePersonError::NoName)
+                }
+            } else {
+                Err(ParsePersonError::BadLen)
+            }
+        } else {
+            Err(ParsePersonError::Empty)
+        }
     }
 }
 
